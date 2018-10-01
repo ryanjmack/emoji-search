305462f18a02e0e9b18494d5d243f308ea3ca2fe
Ryan <ryanjmack21@gmail.com>
2018-10-01 11:06:51 -0400

    Add App header and general styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import emojis from './emojis.json';
 
 
 class App extends React.Component {
+
   render() {
     return (
       <div className="App">
-        <h1>Hello world!</h1>
+        <h1>
+          <span role="img" aria-label="emoji gorilla">🦍</span> Emoji Search <span role="img" aria-label="emoji gorilla">🦍</span>
+        </h1>
       </div>
     )
   }
