4b47035fa68861a8226952a01dc9a0220835f88c
Ryan <ryanjmack21@gmail.com>
2018-10-02 08:46:28 -0400

    Add String.prototype.includes polyfill for IE suppport

--- src/index.js
+++ src/index.js
@@ -9,6 +9,23 @@ document.body.classList.add('body');
 document.querySelector('html').classList.add('html');
 
 
+// Why am I not surprised...
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes#Browser_compatibility
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
