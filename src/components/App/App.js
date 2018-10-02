1384d308a90e43e34ea7d11b48c62fad96bce7cd
Ryan <ryanjmack21@gmail.com>
2018-10-02 08:45:11 -0400

    Add Search functionality. The search is passed a handler that can mutate state and update the display

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import './App.css';
+import Search from '../Search/Search';
 import EmojiList from '../EmojiList/EmojiList';
 import emojis from './emojis.json';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // the initial state is all of the emojis
+    this.state = ({
+      results: emojis
+    });
+
+    this.returnSearchResults = this.returnSearchResults.bind(this);
+  }
+
+
+  // handler that we'll pass to the Search component
+  returnSearchResults(emojis) {
+    this.setState({
+      results: emojis
+    })
+  }
+
 
   render() {
     return (
       <div className="App">
-        <h1>Emoji Search</h1>
-        <EmojiList emojis={emojis} />
+        <h1>
+          <span role="img" aria-label="ghost emoji">👻</span> Emoji Search <span role="img" aria-label="ghost emoji">👻</span>
+        </h1>
+        <Search emojis={emojis} returnSearchResults={this.returnSearchResults} />
+        <EmojiList emojis={this.state.results} />
       </div>
     )
   }
